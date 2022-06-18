deeab4063177b8b99a1daa4d14edcdb0f17f1fd6
MihailDimitrov1235 <107298750+MihailDimitrov1235@users.noreply.github.com>
2022-06-18 21:37:37 +0300

    explosions

--- particles.py
+++ particles.py
@@ -16,14 +16,14 @@ class moneyEarned:
         self.timer -= 1
         return self.timer < 0
 
-class explosion:
+class asteroidExplosion:
     def __init__(self,timeBetween,x,y,size):
         self.explosionStage = 1
         self.timeBetween = timeBetween
-        self.x = x
-        self.y = y
         self.size = size
         self.image = pygame.transform.scale(pygame.image.load('Assets/explosions/Explosion1.png'), (32*self.size, 32*self.size))
+        self.x = x - self.size*16
+        self.y = y - self.size*16
         self.timer = 0
     def draw(self,surface):
         surface.blit(self.image,(self.x,self.y))
@@ -32,8 +32,10 @@ class explosion:
             self.timer = 0
             self.explosionStage += 1
             if self.explosionStage<=8:
-                self.image = pygame.transform.scale(pygame.image.load(f'Assets/explosions/Explosion{self.explosionStage}.png'), (32, 32))
+                self.image = pygame.transform.scale(pygame.image.load(f'Assets/explosions/Explosion{self.explosionStage}.png'), (32*self.size, 32*self.size))
                 return False
             else:
                 return True
         return False
+class bulletExplosions:
+    def __init__(self):
