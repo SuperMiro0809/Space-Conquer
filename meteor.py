87ca4918dfaaa942ffad787f60a27cc467d3368d
MihailDimitrov1235 <107298750+MihailDimitrov1235@users.noreply.github.com>
2022-06-18 21:38:06 +0300

    explosions

--- meteor.py
+++ meteor.py
@@ -1,23 +1,25 @@
 import pygame
 import main
 
+
 class Meteor:
-    def __init__(self,x,y,multiplier):
-        self.image = pygame.transform.scale(main.METEOR_IMAGE,(84+multiplier*2-10,68+multiplier*2-10))
+    def __init__(self, x, y, multiplier):
+        self.width = 84 + multiplier * 2 - 10
+        self.height = 68 + multiplier * 2 - 10
+        self.image = pygame.transform.scale(main.METEOR_IMAGE, (self.width, self.height))
         self.x = x
         self.y = y
-        self.vel = 6
+        self.vel = 3
         self.multiplier = multiplier
-        self.hitbox = (self.x + 6, self.y + 6, 80+multiplier*2-10, 62+multiplier*2-10)
+        self.hitbox = (self.x + 6, self.y + 6, self.width, self.height)
         self.health = self.multiplier
-    
+
     def draw(self, surface):
         if main.run:
-            surface.blit(self.image,(self.x,self.y))
-            self.hitbox = pygame.Rect(self.x + 6, self.y + 6, 80+self.multiplier*2-10, 62+self.multiplier*2-10)
+            surface.blit(self.image, (self.x, self.y))
+            self.hitbox = pygame.Rect(self.x, self.y,self.width,self.height)
             pygame.draw.rect(surface, (255, 0, 0), self.hitbox, 2)
 
     def takeDmg(self):
         self.health -= main.shipDmg
-        return self.health<=0
-
+        return self.health <= 0
